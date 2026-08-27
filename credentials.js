ad58b3b0acce17dc44c5c51eef499617362a658d
rg-sulselraya <rg-sulselraya@users.noreply.github.com>
2026-08-27 22:52:25 +0800

    Restore internal branch password login

---
+++ credentials.js
@@ -0,0 +1,24 @@
+window.dashboardCredentials = {
+  "salt": "90eea3eb762c9b411746621c2f8dac3f",
+  "iterations": 120000,
+  "adminPasswordHash": "1fd5625ba4809597ffe4a34045dcd10afeec6ac8c0b304bdc8583751bcf33350",
+  "branchPasswordHashes": {
+    "0842bd36a9b69fe7e60b08bc357d23be3511d81bbc35a01d87f8ef73ee7826dd": "Bone - Ahmad Yani",
+    "9ac5a5dd4c72a4a731a523ae5f386b4e643303070867373edc92b2bd0eb6448c": "Bulukumba - Jend. Sudirman",
+    "54fa14ed0bb6557f39daa20d618f458298a04175878b2fb9b0fe8b4d2a9c438a": "Gowa - Sungguminasa",
+    "b4ed75f0185d513a97b0be20deb6d8148c5990946b07fcc29cecc069a533a7d1": "Makassar - Baruga",
+    "8c38f0444b8df2d5d8f3d7386fbfd7a97fa5af0b156443b1675acc0981184753": "Makassar - Cendrawasih",
+    "797d75d9775cfda3a11d0f3250cfb171b8ead0d561c3d586f037ac9394ff806d": "Makassar - Hertasning",
+    "42a79e11c703fb4d572a50c2023d9b3517bc7183b4fd900617d0f82a436c8e4b": "Makassar - Perintis",
+    "e9fde1e4f94596cb7cc687759af3a835d81eb3b903aeb332b1a5caa21eae04b1": "Makassar - Sudiang",
+    "a452f5bd1de56e035c275b5f609e97d29c1c736c69d6ff794bfce90476512eae": "Palopo - Andi Kambo",
+    "8552cbfe0e38774ecd7812c03d23c7cb84ec6f14f5460a06c2f5faa161591ac9": "Pangkep - Sultan Hasanuddin",
+    "1ff4c1bc9671529cd4867e661e79bab7e18763ec955dbad0b8a8627932a1e7aa": "Parepare - Mattirotasi",
+    "46f0cd5704b95224031c423fe85a14b9438fffb11a6f5dd2805543ea8653854d": "Pinrang - Jend. Sudirman",
+    "bd2bac4607aeec7187b69b31a82253da58f3e9c971be89fd2323670e49a356ae": "Sidrap - Jenderal Sudirman",
+    "a42abc83d6e593c0d69d1fde6f8e7ba9af698277342dd88bba9ad7842ad54ae8": "Soppeng - Lalabata",
+    "a504a28585f0a39d0858242221e678288bb753694d13219eb931538c10a0f8a7": "Tana Toraja - Makale",
+    "e0a957e78c35a08ab0d0742db5cf29d7565425ef722a9c0a5092b5356ae65ed3": "Toraja Utara - Poros Bolu",
+    "ebed100852ff2cb8a58e3467d566432856347eb00c0525783fc789c5b73bb04c": "Wajo - Jend. Sudirman"
+  }
+};
